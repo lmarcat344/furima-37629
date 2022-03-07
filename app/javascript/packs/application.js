1be4a0ba918cd881304f31b2e44aa198bcf9de0b
lmarcat344 <lmarcat344@gmail.com>
2022-03-07 23:30:50 +0900

    その他の事前準備

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start() //ページにアクセスしても、JavaScriptが作動しない場合を排除
 require("@rails/activestorage").start()
 require("channels")
 

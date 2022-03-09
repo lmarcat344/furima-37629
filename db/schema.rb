0d881ab295127fbe23f168dba1c89394f3d946a8
lmarcat344 <lmarcat344@gmail.com>
2022-03-09 20:54:50 +0900

    userデータベースに保存出来るようにした

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_084832) do
+ActiveRecord::Schema.define(version: 2022_03_09_112011) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "password", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
     t.string "last_name_zenkaku", null: false
     t.string "first_name_zenkaku", null: false

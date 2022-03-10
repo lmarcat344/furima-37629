65c3c660eeddc6655ba6397d6ffb0305747ebe27
lmarcat344 <lmarcat344@gmail.com>
2022-03-10 21:51:59 +0900

    修正２−１ 全角カナのテストで”ー”有りで通るようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
     validates :nickname
     validates :last_name_zenkaku, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
     validates :first_name_zenkaku, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: 'is invalid. Input full-width katakana characters' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: 'is invalid. Input full-width katakana characters' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
     validates :birthday
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,15 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-      it ' 全ての項目が存在すれば登録できる' do
+      it '全ての項目が存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+      it 'last_name_kanaが全角(カタカナ)"ー"有りで登録できる' do
+        @user.last_name_kana = 'メアリー'
+        expect(@user).to be_valid
+      end
+      it 'first_name_kanaが全角(カタカナ)"ー"有りで登録できる' do
+        @user.first_name_kana = 'メアリー'
         expect(@user).to be_valid
       end
     end

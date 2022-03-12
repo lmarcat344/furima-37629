ba246c78bf086efabd605fb29047b8a649acde97
lmarcat344 <lmarcat344@gmail.com>
2022-03-12 18:57:32 +0900

    create アクションのルーティグの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new()
+  end
+
+  def create
+    @item = Item.new(item_params)
+    # @item.save
+    
+    redirect_to action: :index
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :content, :category_id, :condition_id, :charge_id, :prefecture_id, :shipping_id, :price).merge(user_id: current_user.id)
+  end
 end

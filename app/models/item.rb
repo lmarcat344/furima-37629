1d95a3e6c14faf90e8bf4b754c5a99b048be91da
lmarcat344 <lmarcat344@gmail.com>
2022-03-12 21:24:25 +0900

    priceのエラーが２重で表示されるのを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,8 +14,8 @@ class Item < ApplicationRecord
     validates :name
     validates :content
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is out of setting range' }
-    validates :price, numericality: { only_integer: true, message: 'is invalid. Input half-width characters' }
   end
+  validates :price, numericality: { only_integer: true, message: 'is invalid. Input half-width characters' }
 
   validates :category_id, numericality: { other_than: 0, message: "can't be blank" } 
   validates :condition_id, numericality: { other_than: 0, message: "can't be blank" } 

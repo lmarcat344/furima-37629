18bc507c129a7b03316696e717b90b56e02e03d3
lmarcat344 <lmarcat344@gmail.com>
2022-03-12 20:27:53 +0900

    バリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,17 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :charge 
   belongs_to :shipping
+
+  with_options presence: true do 
+    validates :name
+    validates :content
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is out of setting range' }
+    validates :price, numericality: { only_integer: true, message: 'is invalid. Input half-width characters' }
+  end
+
+  validates :category_id, numericality: { other_than: 0, message: "can't be blank" } 
+  validates :condition_id, numericality: { other_than: 0, message: "can't be blank" } 
+  validates :charge_id, numericality: { other_than: 0, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :shipping_id, numericality: { other_than: 0, message: "can't be blank" }  
 end

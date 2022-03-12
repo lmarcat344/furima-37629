715361e0a933c8cf704f86083c7bfd63a834521b
lmarcat344 <lmarcat344@gmail.com>
2022-03-12 20:21:54 +0900

    アソシエーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions 
+  belongs_to :prefecture
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :charge 
+  belongs_to :shipping
 end

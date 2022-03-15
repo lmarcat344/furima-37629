aec06c6d0a918e0c6178c564d9fd2cfb53cf1aee
lmarcat344 <lmarcat344@gmail.com>
2022-03-15 20:57:25 +0900

    アソシエーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   has_many :items
+  has_many :orders
 
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
   with_options presence: true do

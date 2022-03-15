01ccd8520bd4d56805aaed43ef13a2ca2a84ddb3
lmarcat344 <lmarcat344@gmail.com>
2022-03-15 22:06:51 +0900

    カード情報からトークンを取得できるよう実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,28 @@
 const pay = () => {
-  console.log("カード情報トークン化のためのJavaScript");
+  const submit = document.getElementById("button");
+  Payjp.setPublicKey("pk_test_431b490651344ae74043b9cf")
+
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order_address[number]"),
+      cvc: formData.get("order_address[cvc]"),
+      exp_month: formData.get("order_address[exp_month]"),
+      exp_year: `20${formData.get("order_address[exp_year]")}`
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+
+  });
 };
 
 window.addEventListener("load", pay);

33ab9dffe183e23a330b3fc41bb52f5eca29be72
lmarcat344 <lmarcat344@gmail.com>
2022-03-16 09:33:50 +0900

    売却済の商品の編集画面へ遷移できないよう実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,8 +51,8 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless current_user.id == @item.user.id
-      redirect_to action: :index
+    unless current_user.id == @item.user.id && @item.order == nil 
+        redirect_to action: :index
     end
   end
 end

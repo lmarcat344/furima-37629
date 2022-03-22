51c1f7a0e3a81c79de312c84aa1bc2e0865029db
lmarcat344 <lmarcat344@gmail.com>
2022-03-22 12:52:13 +0900

    出品時と編集時プレビューできるようにする

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,7 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
   const postForm = document.getElementById('item-name')
 
-  if(!postForm) return null;
+  if(!postForm) return null; // document読み込み時のエラーを回避
 
-  console.log("preview.js")
+  // プレビューを表示するスペースを取得「
+  const previewList = document.getElementById('previews');
+
+  // 画像のファイル選択をしたときイベントが駆動する
+  const fileField = document.querySelector('input[type="file"][name="item[image]"]')
+  fileField.addEventListener('change', function(e){
+    // 古いプレビューがあった場合は消す
+    const alreadyPreview = document.querySelector('.preview');
+    if(alreadyPreview) {
+      alreadyPreview.remove();
+    }
+    
+    // 画像ファイルのURLを取得
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+    // 画像要素を作る
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+    // 取得した画像を表示させる
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
+  })
 })
